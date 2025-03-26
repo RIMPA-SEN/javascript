0d7bcbed1ac32a990b8f00d88591e675db95e582
RIMPA-SEN <senrimpa625@gmail.com>
2025-03-26 17:01:22 +0530

    Operators

---
+++ Operators.js
@@ -0,0 +1,65 @@
+//Arithmetic operators
+
+let a= 5;
+let b =2;
+console.log("a+b=",a+b); //addition
+let c = a-b;
+console.log("a-b=",c); //subtraction
+console.log("a*b=",a*b); //multiply
+console.log("a/b=",a/b); //Divide
+console.log("a%b=",a%b); //Modulus
+console.log("a**b=",a**b); //Exponential
+
+//Unary Operators-one operand
+console.log("a++=",a++); //5
+console.log(a); //6
+console.log("a--=",a--); //6
+console.log(a); //5
+
+console.log("--a=",--a); //4
+console.log(a); //4
+console.log("++a=",++a) //5
+console.log(a); //5
+
+
+//Assignment Operators
+console.log("a+=",a+=4);
+console.log("a-=",a-=4);
+console.log("a*=",a*=4);
+console.log("a/=",a/=4);
+console.log("a**=",a**=4);
+console.log("a%=",a%=4);
+
+
+//Compararison Operators
+console.log("a==b=", a==b);
+console.log("a!=b=", a!=b);
+b="5";
+console.log("a==b=", a==b);
+console.log("a!=b=", a!=b);
+console.log("a===b=", a===b);
+console.log("a!==b=", a!==b);
+
+b=3
+console.log("a>b=",a>b);
+console.log("a<b=",a<b);
+console.log("a>=b=",a>=b);
+console.log("a<=b=",a<=b);
+
+
+//Logical Operators
+let cond1 = a>b;
+let cond2= b===2;
+console.log("cond1&&cond2=", cond1&&cond2);
+b=3;
+console.log("cond1||cond2=", cond1||cond2);
+console.log(!cond1);
+
+
+
+
+
+
+
+
+
