e055d1d0148e4002571b3687c8fa0027ed40b718
RIMPA-SEN <senrimpa625@gmail.com>
2025-03-26 17:26:53 +0530

    Conversions

---
+++ Conversion.js
@@ -0,0 +1,81 @@
+/*let score = "33";
+console.log(score);
+console.log(typeof(score));
+
+let value = Number(score);
+console.log(value);
+console.log(typeof(value)); 
+
+let score = "33abc";
+console.log(score);
+console.log(typeof(score));
+
+let value = Number(score);
+console.log(value);
+console.log(typeof(value)); 
+
+let score = null;
+console.log(score);
+console.log(typeof(score));
+
+let value = Number(score);
+console.log(value);
+console.log(typeof(value)); 
+
+let score = undefined;
+console.log(score);
+console.log(typeof(score));
+
+let value = Number(score);
+console.log(value);
+console.log(typeof(value)); 
+
+let score = "Rimpa";
+console.log(score);
+console.log(typeof(score));
+
+let value = Number(score);
+console.log(value);
+console.log(typeof(value)); 
+
+let score = true;
+console.log(score);
+console.log(typeof(score));
+
+let value = Number(score);
+console.log(value);
+console.log(typeof(value)); 
+
+// "33" --> 33 , Undefine and "33abc" and "Rimpa"-->NaN, true--> 1, null --> 0
+
+let score = 1;
+console.log(score);
+console.log(typeof(score));
+
+let value = Boolean(score);
+console.log(value);
+console.log(typeof(value)); 
+
+
+let score = "";
+console.log(score);
+console.log(typeof(score));
+
+let value = Boolean(score);
+console.log(value);
+console.log(typeof(value)); 
+
+let score = "rimpa";
+console.log(score);
+console.log(typeof(score));
+
+let value = Boolean(score);
+console.log(value);
+console.log(typeof(value)); */
+
+//1 --> true, "" -->false , "rimpa"-->true
+
+
+
+
+
