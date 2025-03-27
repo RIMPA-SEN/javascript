a3c92c53f0f176b673ba65b46cb52200c566b76d
RIMPA-SEN <senrimpa625@gmail.com>
2025-03-27 15:26:33 +0530

    Types Of Variables

---
+++ variables.js
@@ -0,0 +1,17 @@
+console.log("Variables in Javascript");
+name = "Anil Patamber";
+console.log(name); //String
+age=27
+console.log(age); //Number
+price= 157.6
+console.log(price); //Float
+x=null
+console.log(null); //null
+y=undefined
+console.log(y);  //undefined
+boolean=false;
+console.log(boolean) //boolean
+
+//dynamically typed any variable can be assigned to any value type
+name=34;
+console.log(name);
