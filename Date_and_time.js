3310cc660cf5b5cb1395637d56e51f1c89c719d3
RIMPA-SEN <senrimpa625@gmail.com>
2025-03-27 08:59:52 +0530

    Date And Time

---
+++ Date_and_time.js
@@ -0,0 +1,14 @@
+//Date is an object in js. javascript stores date as number of milliseconds since Januray 01 1970
+
+const myDate = new Date();
+console.log(myDate);
+
+//Date methods
+console.log(myDate.toString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toUTCString());
+console.log(myDate.getHours());
+
+
