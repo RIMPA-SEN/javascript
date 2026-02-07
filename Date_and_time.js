6818244626f7109301e085a7b0f556d25487f576
Rimpa <43918145+RIMPA-SEN@users.noreply.github.com>
2026-02-07 10:41:50 +0530

    Enhance Date_and_time.js with more console logs
    Added additional console logs to display date information and types.

--- Date_and_time.js
+++ Date_and_time.js
@@ -1,14 +1,27 @@
 //Date is an object in js. javascript stores date as number of milliseconds since Januray 01 1970
 
 const myDate = new Date();
-console.log(myDate);
+console.log(myDate); 
+console.log(typeof myDate); //Object
 
 //Date methods
-console.log(myDate.toString());
-console.log(myDate.toLocaleDateString());
-console.log(myDate.toLocaleString());
+console.log(myDate.toString()); //Wed Mar 01 2023 12:05:19 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toLocaleDateString()); 
+console.log(myDate.toDateString()); //Wed Mar 01 2023
+console.log(myDate.toLocaleString()); //3/1/2023 , 12:05:!9 pm
 console.log(myDate.toLocaleTimeString());
 console.log(myDate.toUTCString());
 console.log(myDate.getHours());
+
+let myCreatedDate = new Date(2023,0,23)  //month starts from 0
+console.log(myCreatedDate.toString()); //Mon Jan 23 2023
+
+let myCreatedDate = new Date(2023,0,23,5,3)  //month starts from 0
+console.log(myCreatedDate.toLocaleString()); //1/23/2023, 5:03:00 AM
+
+
+
+
+//Temporal is coming to replace Date
 
 
